9ff84c5eb030691918057a8478b4895b05f72810
amipatel14 <32078080+amipatel14@users.noreply.github.com>
2018-11-10 16:08:51 -0600

    Coin Script/Death(WIP)
    added "Coin" and "SpawnCoin Script"
    Death scrpit added to DeathTrigger in the game

---
+++ Assets/Scripts/SpawnCoin.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCoin : MonoBehaviour
+{
+    [SerializeField] private Transform[] coinSpawns;
+    [SerializeField] private GameObject coin;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Spawn();
+	}
+	
+	// Update is called once per frame
+	void Spawn ()
+    {
+        for (int i = 0; i < coinSpawns.Length; i++)
+        {
+            int coinFlip = Random.Range(0, 2);
+            if (coinFlip > 0)
+            {
+                Instantiate(coin, coinSpawns[i].position, Quaternion.identity);
+            }
+        }		
+	}
+}
