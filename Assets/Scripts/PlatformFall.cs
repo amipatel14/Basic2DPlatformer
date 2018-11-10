c6be5c6e6a6c6531d82665bbd3494009de134c00
amipatel14 <32078080+amipatel14@users.noreply.github.com>
2018-11-10 15:39:48 -0600

    PlatformFall
    added Script "PlatformFall"
    added RigidBody2D to the ground, made it kinematic
    Changed Background color
    made material for the ground
    added coin sprite

--- Assets/Scripts/PlatformFall.cs
+++ Assets/Scripts/PlatformFall.cs
@@ -2,15 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlatformFall : MonoBehaviour {
+public class PlatformFall : MonoBehaviour
+{
+    [SerializeField] private float fallDelay;
 
+    private Rigidbody2D rb2d;
 	// Use this for initialization
-	void Start () {
-		
+	void Awake ()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+            Invoke("Fall", fallDelay);
+    }
+
+    void Fall()
+    {
+        rb2d.isKinematic = false;
+    }
+
 }
