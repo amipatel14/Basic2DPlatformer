6c831b9836f9061c8c0da5029a76b4c1dadef9d4
amipatel14 <32078080+amipatel14@users.noreply.github.com>
2018-11-10 14:21:07 -0600

    SpawnManager
    Added "SpawnManager" Script:
    creates random platforms
    Added gameObject SpawnManager
    Random platforms added

--- Assets/Scripts/SimplePlatformController.cs
+++ Assets/Scripts/SimplePlatformController.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SimplePlatformController : MonoBehaviour {
-    
+public class SimplePlatformController : MonoBehaviour
+{
 
     [SerializeField] private float moveForce = 365f;
     [SerializeField] private float maxSpeed = 5f;
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] private int maxPlatforms = 20;
+    [SerializeField] GameObject platform;
+    [SerializeField] private float horizontalMin = 6.5f;
+    [SerializeField] private float horizontalMax = 14f;
+    [SerializeField] private float verticalMin = -6f;
+    [SerializeField] private float verticalMax = 6f;
+
+    private Vector2 originalPosition;
+
+	// Use this for initialization
+	void Start ()
+    {
+        originalPosition = transform.position;
+        Spawn();
+	}
+
+    void Spawn()
+    {
+        for (int i = 0; i < maxPlatforms; i++)
+        {
+            Vector2 randomPosition = originalPosition + new Vector2(Random.Range(horizontalMin, horizontalMax), Random.Range(verticalMin, verticalMax));
+            Instantiate(platform, randomPosition, Quaternion.identity);
+            originalPosition = randomPosition;
+        }
+    }
+}
