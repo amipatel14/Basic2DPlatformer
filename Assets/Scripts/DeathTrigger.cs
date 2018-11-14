8ea1dedf328b5bb7cbc5cdf629b157a65c293b7b
Ami Patel <Ami.Patel3@iam.local>
2018-11-14 14:20:31 -0600

    Death Respawn
    Load the same scene when player dies
    
    SceneManager.LoadScene(SceneManager.GetActiveScene().name);

--- Assets/Scripts/DeathTrigger.cs
+++ Assets/Scripts/DeathTrigger.cs
@@ -15,11 +15,11 @@ public class DeathTrigger : MonoBehaviour {
 		
 	}
 
-    void OnTriggerEnter(Collider other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
 }
